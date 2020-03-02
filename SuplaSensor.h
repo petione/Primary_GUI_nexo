7d4ce6b97b50ecae82364b7bcbedfdbc08cd0db8
krystianmen <krystian.franieczek@gmail.com>
2020-03-02 19:38:13 +0100

    test

--- SuplaSensor.h
+++ SuplaSensor.h
@@ -1,17 +1,41 @@
 #ifndef SuplaSensor_h
 #define SuplaSensor_h
 
-#include <supla/sensor/DS18B20.h>
+#include <Arduino.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
 
-class DS18B20 : public Supla::Sensor::DS18B20 {
+#include <supla-common/log.h>
+#include <supla/sensor/thermometer.h>
+
+class OneWireBus {
   public:
-    DS18B20(uint8_t pin, uint8_t *deviceAddress = nullptr)
-      : Supla::Sensor::DS18B20(pin, deviceAddress) {};
-      
-    uint8_t  getPin() {
-      return myBus->pin;
-    }
+    OneWireBus(uint8_t pinNumber);
+    int8_t getIndex(uint8_t *deviceAddress);
 
+    uint8_t pin;
+    OneWireBus * nextBus;
+    unsigned long lastReadTime;
+    DallasTemperature sensors;
+
+  protected:
+    OneWire oneWire;
 };
 
+class DS18B20 : public Thermometer() {
+  public:
+    DS18B20(uint8_t pin, uint8_t *deviceAddress = nullptr);
+    void iterateAlways();
+    double getValue();
+    void onInit();
+
+  protected:
+    static OneWireBus *oneWireBus;
+    OneWireBus *myBus;
+    DeviceAddress address;
+    int8_t retryCounter;
+    double lastValidValue;
+};
+
+//OneWireBus *DS18B20::oneWireBus = nullptr;
 #endif //SuplaSensor_h
