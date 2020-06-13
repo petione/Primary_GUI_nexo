fa653074e254821cab2ab86803cf463d4957062f
krystianmen <krystian.franieczek@gmail.com>
2020-06-13 07:11:27 +0200

    commit

--- SuplaConfigESP.h
+++ SuplaConfigESP.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) AC SOFTWARE SP. Z O.O.
+  Copyright (C) krycha88
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License
